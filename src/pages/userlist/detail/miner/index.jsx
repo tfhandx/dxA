27cc6d6fc4963bf2f931c2f19716c8d2abd105f4
Tengfei Han <tfhan@dxhain.com>
2020-04-30 18:16:47 +0800

    iframe

--- src/pages/userlist/detail/miner/index.jsx
+++ src/pages/userlist/detail/miner/index.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import styles from './index.less'
+import React, { useEffect, useRef, } from 'react';
+import ReactDOM from 'react-dom';
+import { history } from 'umi';
+import { Button } from 'antd'
+import styles from './index.less';
+const subroot = document.getElementById('subroot');
+const root = document.getElementById('root');
+const el = React.createRef()
+el.current = document.createElement('div')
 const MinerDetail = ({ ...props }) => {
-    return <div height='100vh' className={styles.iframe}>
-        <iframe
-            src="https://www.baidu.com/"
-            frameborder="0"
-            scrolling='auto'
-            name='shequ'
-            width='100%'
-            style={{ height: '100vh' }}
-        ></iframe>
-    </div>
+    useEffect(() => {
+        subroot.appendChild(el.current)
+        subroot.style.display = 'block'
+        return () => {
+            subroot.removeChild(el.current);
+            subroot.style.display = 'none'
+        }
+    }, [])
+    return <iframe
+        src="http://localhost:8080/detailed?key=pledge"
+        frameBorder="0"
+        scrolling='auto'
+        name='shequ'
+        width='100%'
+        style={{ height: '100vh' }}
+    ></iframe>
+    // return ReactDOM.createPortal(
+    //     <div height='100vh' className={styles.iframe} >
+    //         <div className={styles.close}>
+    //             <Button onClick={() => {
+    //                 subroot.style.display = 'none';
+    //                 history.goBack();
+    //                 history.push({
+    //                     pathname: '/userlist/detail/basic',
+    //                     query: {
+    //                         a: 'b',
+    //                     },
+    //                 });
+    //             }}>x</Button>
+    //         </div>
+    //         <iframe
+    //             src="http://localhost:8080/detailed?key=pledge"
+    //             frameborder="0"
+    //             scrolling='auto'
+    //             name='shequ'
+    //             width='100%'
+    //             style={{ height: '100vh' }}
+    //         ></iframe>
+    //     </div>,
+    //     el.current
+    // );
 }
 export default MinerDetail;
