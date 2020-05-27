5385dd023ec6338c4af01d0b8b545731d3f07241
Tengfei Han <tfhan@dxhain.com>
2020-05-27 19:47:30 +0800

    init

--- src/pages/user/login/index.js
+++ src/pages/user/login/index.js
@@ -2,7 +2,7 @@ import { dynamic } from 'umi';
 export default dynamic({
     loader: async function () {
         // 这里的注释 webpackChunkName 可以指导 webpack 将该组件 HugeA 以这个名字单独拆出去
-        const { default: AuthorityIndex } = await import(/* webpackChunkName: "external_A" */ './dynamic');
-        return AuthorityIndex;
+        const { default: Login } = await import(/* webpackChunkName: "external_A" */ './dynamic');
+        return Login;
     },
 });
