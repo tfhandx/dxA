da8168eafd27bfb0b17252dc30e9fab13ba9d684
Tengfei Han <tfhan@dxhain.com>
2020-05-27 13:15:15 +0800

    test enviriment

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,8 +3,8 @@ let baseUrl = "https://shequ.dxchain.com";
 if (process.env.NODE_ENV === "production") {
   baseUrl = "https://shequ.dxchain.com";
 } else if (process.env.NODE_ENV === "development") {
-  // baseUrl = "https://shequ-test.dxchain.com";
-  baseUrl = "http://localhost:8081";
+  baseUrl = "https://shequ-test.dxchain.com";
+  // baseUrl = "http://localhost:8081";
 } else if (process.env.NODE_ENV === "development2") {
   baseUrl = "https://shequ-test2.dxchain.com";
   // baseUrl = "http://192.168.31.118:8081";
